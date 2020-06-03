410432d2485d0a6ff84ef7744836e56769cf9a66
Larry Battle <larry.battle.tech@gmail.com>
2020-06-03 01:28:46 -0500

    frontend: removed --verbose from ng build

--- frontend/build_to_service.sh
+++ frontend/build_to_service.sh
@@ -1,3 +1,3 @@
 
 ## ng is from -> npm i @angular/cli
-ng build --verbose --prod --source-map --aot --base-href '/ui/' --deploy-url '/ui/' --delete-output-path --output-path ../service/src/main/resources/static/ui
+ng build --prod --source-map --aot --base-href '/ui/' --deploy-url '/ui/' --delete-output-path --output-path ../service/src/main/resources/static/ui
