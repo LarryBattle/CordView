d8563e008fc06549a4298f2416ea18fae3793537
Larry Battle <larry.battle.tech@gmail.com>
2020-06-15 02:40:23 -0500

    Removed unneeded @Repository from *.Repository.java

--- service/src/main/java/dev/larrybattle/cordview/repository/JobPostingRepository.java
+++ service/src/main/java/dev/larrybattle/cordview/repository/JobPostingRepository.java
@@ -2,10 +2,7 @@ package dev.larrybattle.cordview.repository;
 
 import dev.larrybattle.cordview.entity.JobPosting;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-// TODO Find out if the Repository is needed.
-@Repository
 public interface JobPostingRepository extends JpaRepository<JobPosting, Long> {
     JobPosting findByGlobalId(String globalId);
 }
