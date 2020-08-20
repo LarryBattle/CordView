86f286106088d346c66e8fd4e61bb4f04d88a3f5
Larry <etest@example.com>
2020-08-19 23:49:31 -0500

    server: FooServiceHealthIndicator.java: Added more example code for a custom health indicator

--- service/src/main/java/dev/larrybattle/cordview/actuator/FooServiceHealthIndicator.java
+++ service/src/main/java/dev/larrybattle/cordview/actuator/FooServiceHealthIndicator.java
@@ -2,7 +2,12 @@ package dev.larrybattle.cordview.actuator;
 
 import org.springframework.boot.actuate.health.Health;
 import org.springframework.boot.actuate.health.HealthIndicator;
+import org.springframework.boot.actuate.health.Status;
 import org.springframework.stereotype.Component;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 // url: https://docs.spring.io/spring-boot/docs/current/reference/html/production-ready-features.html#production-ready-health
 // {
@@ -27,15 +32,29 @@ import org.springframework.stereotype.Component;
 //    }
 //  }
 //}
+
+// $ curl -I -L http://localhost:8080/actuator/health/fooService
+//  % Total    % Received % Xferd  Average Speed   Time    Time     Time  Current
+//                                 Dload  Upload   Total   Spent    Left  Speed
+//  0     0    0     0    0     0      0      0 --:--:-- --:--:-- --:--:--     0HTTP/1.1 412
+//Content-Type: application/vnd.spring-boot.actuator.v3+json
+//Transfer-Encoding: chunked
+//Date: Thu, 20 Aug 2020 04:45:26 GMT
+
 // TOOD Remove this class
 @Component
 public class FooServiceHealthIndicator implements HealthIndicator {
 
     @Override
     public Health health() {
-        return Health.down()
-                .withDetail("Test", "value")
-                .withDetail("fun", true)
+        Map<String, Object> props = new HashMap<>();
+        props.put("propA", "valueA");
+        props.put("isPropB", true);
+        props.put("timeForPropB", new Date());
+
+        return Health.status("TEST_FAILED")
+                .withDetails(props)
                 .build();
     }
+
 }
